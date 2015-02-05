091b7e9368d548fa2180a807d8a222e5880a2261
Mike Randrup <github@mikerandrup.com>
2015-02-05 12:55:58 -0600

    Initial blank project commit.

---
+++ GolfScoreSPA/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SlidingCardsDemo.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Default()
+        {
+            return View("~/Views/Default.cshtml");
+        }
+    }
+}
---
+++ GolfScoreSPA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SlidingCardsDemo.MvcApplication" Language="C#" %>
